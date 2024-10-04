cc3201c908d88fe282334de6647cb8a23ed2ef65
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-04 08:22:17 +0000

    update

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/*
+ * Return a Promise using this prototype function getResponseFromAPI()
+ */
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The fake API is not working currently'));
+  });
+}
+
+export default getFullResponseFromAPI;
