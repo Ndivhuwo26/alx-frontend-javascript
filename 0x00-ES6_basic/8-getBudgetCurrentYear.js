a21fee78af9d21fa0ebfc06d0948df73894ed197
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-04 07:54:17 +0000

    update

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,4 +1,6 @@
-//ewrite the getBudgetForCurrentYear function to use ES6 computed property names on the budget object
+/*
+ * Rewrite the getBudgetForCurrentYear function to use ES6 computed property names on the budget object
+ */
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
