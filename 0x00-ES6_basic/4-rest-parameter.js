9b0fbca33a781fe66edd4b02c30b8a48606dc4cd
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-04 07:39:54 +0000

    update

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,6 @@
-//function to return the number of arguments passed to it using the rest parameter syntax
+/*
+ *function to return the number of arguments passed to it using the rest parameter syntax
+ */
 export default function returnHowManyArguments(...theArgs) {
   return theArgs.length;
 }
