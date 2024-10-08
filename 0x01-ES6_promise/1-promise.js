21dbd9b5fd7d320c2571345f04c436afa786f7fb
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-08 06:47:28 +0000

    update

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,17 @@
+/*
+ * Using the prototype below, return a promise
+ */
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
